c6e39abc90845804c0afd63ef69708042c1cc0fd
unknown <hassanrashid0018@gmail.com>
2024-09-18 18:48:54 +0500

    New Changes Added

--- src/Pages/Modal.js
+++ src/Pages/Modal.js
@@ -19,26 +19,26 @@ const Modal = ({ user, repos, onClose }) => {
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-      <div className="modal-box-custom bg-white p-6 rounded-lg shadow-lg relative max-w-lg w-full overflow-y-auto">
-        <button
-          className="absolute top-2 right-2 text-gray-600 hover:text-gray-900"
-          onClick={onClose}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M6 18L18 6M6 6l12 12"
-            />
-          </svg>
-        </button>
+      <div className="modal-box-custom bg-white p-6 rounded-lg shadow-lg relative max-w-lg w-full overflow-y-auto z-50">
+      <button
+  className="absolute top-2 right-2 text-gray-600 hover:text-gray-900 z-50"
+  onClick={onClose}
+>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="h-6 w-6"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      d="M6 18L18 6M6 6l12 12"
+    />
+  </svg>
+</button>
         <div className="flex flex-col items-center">
           <img
             src={user.avatar_url}
@@ -49,7 +49,7 @@ const Modal = ({ user, repos, onClose }) => {
           <p className="text-lg text-gray-600 inset-3 absolute">id no: {user.id}</p>
           <a
             href={user.html_url}
-            className="text-blue-500 hover:underline"
+            className="text-blue-500 hover:underline z-50"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -78,7 +78,7 @@ const Modal = ({ user, repos, onClose }) => {
                 <li key={repo.id} className="mb-4">
                   <a
                     href={repo.html_url}
-                    className="text-blue-500 hover:underline text-lg font-semibold"
+                    className="text-blue-500 hover:underline text-lg font-semibold z-50"
                     target="_blank"
                     rel="noopener noreferrer"
                   >
